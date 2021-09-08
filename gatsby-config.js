0a9be2f106bbdef932887789eb25e5ed146f895e
tiuscia <tiuscia@gmail.com>
2021-09-07 17:46:12 -0700

    variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,10 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        // accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-        // spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // environment: process.env.ENVIRONMENT,
-        // host: 'preview.contentful.com',
         accessToken: process.env.GATSBY_IS_PREVIEW ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         environment: process.env.ENVIRONMENT,
-        host: process.env.GATSBY_IS_PREVIEW ? 'preview.contentful.com' : 'cdn.contentful.com',
+        host: process.env.CONTENTFUL_HOST,
       },
     },
     "gatsby-plugin-styled-components",
