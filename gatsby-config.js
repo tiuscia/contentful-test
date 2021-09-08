227b23361ceb6b06fdab2462eea5c8d2358c4129
Alex Haynes <alexhaynes@helpscout.com>
2021-09-08 13:23:02 -0700

    Add env to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host:process.env.CONTENTFUL_HOST,
+        environment: process.env.ENVIRONMENT,
       },
     },
     "gatsby-plugin-styled-components",
